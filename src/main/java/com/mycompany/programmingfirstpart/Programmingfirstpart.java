7853df48e8d2933dea98d4787661f4fba8d0fbf1
RC_Student_lab <RC_Student_lab@10.0.0.15>
2024-09-26 18:22:47 +0000

    Fifth commit

--- src/main/java/com/mycompany/programmingfirstpart/Programmingfirstpart.java
+++ src/main/java/com/mycompany/programmingfirstpart/Programmingfirstpart.java
@@ -13,41 +13,33 @@ public class Programmingfirstpart {
     public static void main(String[] args) {
         //Declarations
         String username="Aya_1";
-       
+       String Password="Berry@1992!";
          String firstname="Ayanda";
         String lastname="Ngwenya";
         
         //Creating an object of the Scanner
         Scanner sc=new Scanner(System.in);
         
-        //Prompting the user 
-        System.out.println("Enter your username:");
-        String enteredusername=sc.next();
         
-        System.out.println("Enter your password:");
-        String enteredPassword=sc.next();
-        
-        System.out.println("Enter your firstname:");
-         firstname=sc.next();
-        
-        System.out.println("Enter your lastname:");
-         lastname=sc.next();
         //Creating instance of the class 
          Login login=new Login();
+        
+        //Method that returns necessary registration messages 
+        login.Registeruser();
+        
          //Calling the checkusername method
-        boolean result=login.checkusername(enteredusername);
+        boolean result=login.checkusername(username);
         System.out.println(result);
+        
         
         //calling the passwordcomplexity method
         
-       boolean output= login.PasswordComplexity(enteredPassword);
+       boolean output= login.PasswordComplexity(Password);
       System.out.println(output);
       
-        //Method that returns necessary registration messages 
-        String display=login.Registeruser(enteredusername, enteredPassword);
-        System.out.println(display);
+        
         //Method that checks if user has provided the correct username and password
-        String show=login.loginUser(enteredusername, enteredPassword);
+        String show=login.loginUser(username, Password);
         System.out.print(show);
         
         //Method that returns necessary login status
